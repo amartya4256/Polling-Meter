59da7a927d46766e8591fafcb0b391e50d8b44f2
amartya4256 <amartya4256@gmail.com>
2019-02-16 01:38:15 +0530

    done

--- PycharmProjects/Polling Machine/Poll/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    aadharid = StringField('Aadhar ID', validators=[DataRequired()])
-    otp = PasswordField('OTP', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    login = SubmitField('Sign In')
--- PycharmProjects/Polling Machine/Poll/static/node.js
+++
@@ -1,3 +0,0 @@
-var mysql = require('poll_database.db');
-var conn = mysql.createConnection();
-conn.
