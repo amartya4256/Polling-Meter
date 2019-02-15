6a984057f32b4121dd5c46e484c97be585cdd485
amartya4256 <46150646+amartya4256@users.noreply.github.com>
2019-02-16 00:09:35 +0530

    Update way2sms.py

--- PycharmProjects/Polling Machine/Poll/way2sms.py
+++ PycharmProjects/Polling Machine/Poll/way2sms.py
@@ -8,17 +8,17 @@ URL = 'http://www.way2sms.com/api/v1/sendCampaign'
 # get request
 def sendPostRequest(phoneNo, textMessage):
   req_params = {
-  'apikey':'YRIJRGPLJ1YN8ZVL1I7A7RTH323V97PN',
-  'secret':'WSKE4WXSQWOW2NCM',
+  'apikey':'MD4CSLB4X1GDN0WAWMWITXT1DRWXU53Y',
+  'secret':'SK2PUA4B0BEOKF6O',
   'usetype':'stage',
   'phone': phoneNo,
   'message':textMessage,
-  # 'senderid':'+919934785875'
+  # 'senderid':'+917903601431'
   }
   return requests.post(URL, req_params)
 
 # get response
-# response = sendPostRequest(URL,'9934785875', 'message-text' )
+# response = sendPostRequest(URL,'7903601431', 'message-text' )
 """
   Note:-
     you must provide apikey, secretkey, usetype, mobile, senderid and message values
